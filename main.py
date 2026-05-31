7bf91c416b61b975c6123d1d53a1810cc4c6a59c
Rafly Hanggaraksa <115408951+raflyhangga@users.noreply.github.com>
2026-05-31 20:47:56 +0700

    change app name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="DuniaMaskapai",
+    page_title="FlightPick",
     page_icon="static/favicon.png",
     layout="wide",
     initial_sidebar_state="collapsed",
